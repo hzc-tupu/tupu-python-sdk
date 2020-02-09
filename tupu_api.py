48e04a5f4a83eb3e3fd28c34137010ebeca12431
huangzhicong <zhiconghuang@tuputech.com>
2020-02-10 01:21:05 +0800

    add video async api

--- tupu_api.py
+++ tupu_api.py
@@ -26,11 +26,12 @@ result = tupu.api(images=images, is_url=False)
 
 import os
 import random
-import datetime
+import time
 import rsa
 import requests
 import base64
 import json
+import mimetypes
 
 
 TUPU_PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
@@ -44,6 +45,8 @@ WQ4v2JR/BA6gVHV5TwIDAQAB
 class TUPU:
     def __init__(self, secret_id, private_key_path, url='http://api.open.tuputech.com/v3/recognition/'):
         self.__url = url + ('' if url.endswith('/') else '/') + secret_id
+        self.__video_sync_url = url + ('' if url.endswith('/') else '/') + 'video/syncscan/' + secret_id
+        self.__video_async_url = url + ('' if url.endswith('/') else '/') + 'video/asyncscan/' + secret_id
         self.__secret_id = secret_id
         # get private key
         with open(private_key_path) as private_key_file:
@@ -53,10 +56,10 @@ class TUPU:
 
     def __sign(self):
         """get the signature"""
-        self.__timestamp = datetime.datetime.now()
+        self.__timestamp = time.time()
         self.__nonce = random.randint(1 << 4, 1 << 32)
         sign_string = "%s,%s,%s" % (self.__secret_id, self.__timestamp, self.__nonce)
-        self.__signature = base64.b64encode(rsa.sign(sign_string.encode("utf-8"), self.__private_key, 'SHA-256'))
+        self.__signature = base64.b64encode(rsa.sign(sign_string.encode("utf-8"), self.__private_key, 'SHA-256')).decode('utf-8')
 
     def __verify(self, signature, verify_string):
         """verify the signature"""
@@ -88,6 +91,32 @@ class TUPU:
                     continue
                 multiple_files.append(('image', (image_file, open(image_file, 'rb'), 'application/*')))
             response = requests.post(self.__url, data=request_data, files=multiple_files)
+        response_json = json.loads(response.text)
+        if not "error" in response_json:
+            response_json['verify_result'] = self.__verify(response_json['signature'], response_json['json'])
+            response_json['json'] = json.loads(response_json['json'])
+        return response_json
+
+    def video_sync_api(self, video, is_url=False, interval=1, maxFrames=200):
+        if not video:
+            raise Exception('video is required')
+        self.__sign()
+        request_data = {
+            "timestamp": self.__timestamp,
+            "nonce": self.__nonce,
+            "signature": self.__signature,
+            "interval": interval,
+            "maxFrames": maxFrames
+        }
+        response = None
+        if is_url:
+            files = {'video': (None, video)}
+        else:
+            if not os.path.isfile(video):
+                raise Exception('No such file "%s"' % video)
+            mime = mimetypes.guess_type(video)
+            files = {'video': (video, open(video, 'rb'), mime[0])}
+        response = requests.post(self.__video_sync_url, data=request_data, files=files)
         response_json = json.loads(response.text)
         if not "error" in response_json:
             response_json['verify_result'] = self.__verify(response_json['signature'], response_json['json'])
